00f42d9e42dad54529ea92043c7e92bd2f7a29ec
Herobroto <herobrotelpro@gmail.com>
2023-11-06 03:49:51 -0600

    03 Card completada

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,7 +2,7 @@ import "./Card.css";
 
 export default function Card({producto, clickeadoDatos}){
     return(
-        <div>
+        <div className="contenidoCard">
             {producto.map(objeto => {
                 return(
                     <div 
@@ -15,14 +15,9 @@ export default function Card({producto, clickeadoDatos}){
                         </div>
                         <div className="texto">
                             <h2>{objeto.nombre}</h2>
-                            <p>{objeto.calzado}</p>
-                            <div className="footerCard">
-                                <div className="colores">
-
-                                </div>
-                                <div className="precio">
-                                    ${objeto.precio}
-                                </div>
+                            <div className="precio">
+                                <p>{objeto.calzado}</p>
+                                <p>${objeto.precio}</p>
                             </div>
                         </div>
                     </div>
